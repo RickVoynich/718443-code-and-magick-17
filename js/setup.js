73a00b095924035c02ece105516930b572bd36dc
Rick <46853857+RickVoynich@users.noreply.github.com>
2019-06-25 23:16:06 +0300

    Добавляет похожих персонажей

---
+++ js/setup.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var wizardCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var wizardEyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomElement = function (array) {
+  return array[Math.floor((Math.random() * array.length))];
+};
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var wizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+var wizards = [];
+
+for (var i = 0; i < 4; i++) {
+  var wizard = {
+    name: getRandomElement(wizardNames) + ' ' + getRandomElement(wizardSurnames),
+    coatColor: getRandomElement(wizardCoatColor),
+    eyesColor: getRandomElement(wizardEyesColor)
+  }
+
+  wizards.push(wizard);
+};
+
+var renderWizard = function (wizard) {
+  var wizardElement = wizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+};
+similarListElement.appendChild(fragment);
+
+var setupSimilar = userDialog.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
