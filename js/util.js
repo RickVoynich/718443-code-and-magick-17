35e6ded9c840dd377a7836aae4221cac47c45287
Rick <46853857+RickVoynich@users.noreply.github.com>
2019-07-29 00:37:14 +0300

    Обработка возможных ошибок

--- js/util.js
+++ js/util.js
@@ -10,11 +10,25 @@
     return Math.floor(Math.random() * (max - min)) + min;
   };
 
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+
   window.util = {
     getRandomElement: getRandomElement,
     getRandomInt: getRandomInt,
     ESC_KEYCODE: 27,
-    ENTER_KEYCODE: 13
+    ENTER_KEYCODE: 13,
+    errorHandler: errorHandler
   };
 
 })();
